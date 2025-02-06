deb62042139d168c42e893c02f67fe96a3a18d04
Tejas <ttejas220@gmail.com>
2025-02-07 00:23:44 +0530

    frountend: Navbar and themeselector done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,16 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
 import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+  const { theme } = useThemeStore();
   return (
     <>
-      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <div
+        className="min-h-screen bg-base-200 transition-colors duration-300"
+        data-theme={theme}
+      >
         <Navbar />
 
         <Routes>
