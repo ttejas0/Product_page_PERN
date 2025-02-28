00c4a4a6e767a89a6883c1129a55e0109651c69e
Tejas <ttejas220@gmail.com>
2025-02-28 15:23:37 +0530

    edited comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,5 +10,3 @@ export const sql = neon(
   `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 );
 // this sql function we export is used as tagged template litral, which allows us to write SQL more safely
-
-//postgresql://neondb_owner:npg_xnUDsF20uCVM@ep-late-recipe-a8blmyhh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
